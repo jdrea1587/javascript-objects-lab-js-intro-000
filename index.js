2374923e17aea438e305a0cf6f29caef64569159
Joshua Rea <joshdanielrea@gmail.com>
2021-03-07 16:24:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
   object[key]=value;
   return object;
 }
+function deleteFromObjectByKey(object,key){
+  var newObj = Object.assign({},obj)
+}
